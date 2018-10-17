01c8253f038a329e5cb7b787d48febeef2644664
Thanawin Boonpojanasoontorn <Pat@Khemjiras-MacBook-Pro.local>
2018-10-17 10:59:20 +0700

    yus

--- wow1.py
+++ wow1.py
@@ -1,1 +1,26 @@
-print("wow")
+import pyglet, glooey
+print("")
+
+#this is pat's code:
+mainWindow = pyglet.window.Window()
+mainGui = glooey.Gui(mainWindow)
+rows = glooey.VBox()
+mainGui.add(rows)
+
+label = glooey.Label("Hello World")
+rows.add(label)
+
+form = glooey.Form("Enter password for me to steal: ")
+rows.add(form)
+
+button = glooey.Button("This is a scam!")
+def buttclicked(wiget):
+    print(form.text)
+button.push_handlers(on_click=buttclicked)
+rows.add(button)
+
+label.set_text("Some new text")
+
+pyglet.app.run()
+
+#helloworld
